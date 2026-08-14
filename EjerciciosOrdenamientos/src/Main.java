7f4b40bbaaae5d9395aabf715cc84940449d311f
judamaca <juandavidmateus123@hotmail.com>
2026-08-14 09:53:47 -0500

    adelanto de  bubblesort

--- EjerciciosOrdenamientos/src/Main.java
+++ EjerciciosOrdenamientos/src/Main.java
@@ -1,13 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-Public class Main {
-    public static void main(string[] args) {
-        int[] miArray = {70, 12, 19, 45, 11, 33, 26};
+public class Main {
+    public static void main(String[] args) {
+        int[] myArray = {70, 12, 19, 45, 11, 33, 26};
         MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
-        int num = miArray.length
-        System.out.println(num);
 
-        // Prueba github
-
+        misAlgoritmos.bubbleSort(myArray);
+        for (int num: myArray) {
+            System.out.println(num + " ");
+        }
     }
 }
--- EjerciciosOrdenamientos/src/MisAlgoritmos.java
+++ EjerciciosOrdenamientos/src/MisAlgoritmos.java
@@ -11,8 +11,23 @@ public class MisAlgoritmos implements Ordenamientos, Busquedas {
 
     @Override
     public int[] bubbleSort(int[] arrayDesordenado) {
-        int [] num = miArray.lenght()
-        return new int[0];
+        int [] num = miArray.lenght();
+        // 1. Bucle externo (cuenta las pasadas)
+        for (int i = 0 ; i < n-1; i++) {
+
+            // 2. Bucle interno (compara los vecinos)
+            // Usamos (n - 1 - i) porque
+            // en cada pasada el final ya está ordenado y nos ahorramos pasos.
+            for (int j = 0; j < n-1; j++) {
+                // comparaciones o intercambios
+                if (myArray[j] > myArray[j+1]) {
+                    int temporal = myArray[j+1];
+                    myArray[j+1] = myArray [j];
+                    myArray[j] = temporal;
+                }
+
+            }
+        }
     }
 
     @Override
