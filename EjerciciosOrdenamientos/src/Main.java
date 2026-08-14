1109d1e0bc62494b8dffcf191ff8eabded0ff30d
judamaca <juandavidmateus123@hotmail.com>
2026-08-14 09:10:52 -0500

    (prueba github)

--- EjerciciosOrdenamientos/src/Main.java
+++ EjerciciosOrdenamientos/src/Main.java
@@ -2,9 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 Public class Main {
     public static void main(string[] args) {
-        int [] miArray = {70, 12, 19, 45, 11, 33, 26};
-            MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
-            int num = miArray.length
-            System.out.println(num);
+        int[] miArray = {70, 12, 19, 45, 11, 33, 26};
+        MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
+        int num = miArray.length
+        System.out.println(num);
+
+        // Prueba github
+
     }
 }
