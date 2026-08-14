8d2dc3497f2888987f89d5ca2dd5a5bf27980d1e
judamaca <juandavidmateus123@hotmail.com>
2026-08-14 09:04:58 -0500

    creacion taller plantilla

---
+++ EjerciciosOrdenamientos/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+Public class Main {
+    public static void main(string[] args) {
+        int [] miArray = {70, 12, 19, 45, 11, 33, 26};
+            MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
+            int num = miArray.length
+            System.out.println(num);
+    }
+}
